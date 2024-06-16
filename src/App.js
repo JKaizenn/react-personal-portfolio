04065512901bc4db2538aa1db0427dd50f9b52d4
Jessen Forbush <for21011@byui.edu>
2024-06-16 14:57:22 -0600

    Bug Fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Route, Routes } from 'react-router-dom';
 import React from 'react';
-import Header from './components/Header';
-import Home from './components/Home';
-import Resources from './components/Resources';
-import About from './components/About';
-import SitePlan from './components/SitePlan';
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/header";
+import Home from './components/home';
+import Resources from './components/resources';
+import About from './components/about';
+import SitePlan from "./components/site-plan";
+import Footer from "./components/footer"; // Import the Footer component
 import './App.css';
 import './styles/index-styles.css';
 
@@ -14,10 +15,11 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="/siteplan" element={<SitePlan />} />
+        <Route path="about" element={<About />} />
+        <Route path="resources" element={<Resources />} />
+        <Route path="siteplan" element={<SitePlan />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
